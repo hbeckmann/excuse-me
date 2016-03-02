4f0d83c2e5546b766424cfcb5cd100f1eca49d61
hbeckmann <hunterbeckmann@gmail.com>
2016-03-01 18:23:19 -0700

    API start

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
+var Firebase = require('firebase');
+var ref = new Firebase("https://excuser.firebaseio.com/");
 
-app.get('/', function(req,res){
-  res.send('Hello! Welcome the Excuse Generator! More Coming soon.');
+
+app.get('/excuse', function(req,res){
+
+  var excuse;
+  ref.once("value", function(snapshot){
+    excuse = snapshot.val();
+      res.send(excuse);
+  }, function (errorObject) {
+    console.log(errorObject);
+    res.end();
+  });
+
 });
+
+
+app.use(express.static('source'));
 
 var port = 8000;
 app.listen(process.env.PORT || port);
